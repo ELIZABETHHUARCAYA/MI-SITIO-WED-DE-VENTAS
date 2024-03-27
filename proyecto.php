4043728974f9fcf4cbf735c0201af5d109d945eb
ELIZABETHHUARCAYA <165177791+ELIZABETHHUARCAYA@users.noreply.github.com>
2024-03-27 09:57:51 -0500

    Add files via upload

---
+++ proyecto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saludo en PHP</title>
+</head>
+<body>
+    <?php
+        // Código PHP para mostrar un mensaje
+        echo "<h1>Hola, mundo cruel!</h1>";
+    ?>
+</body>
+</html>
